acc3ec8b1fb1c7dd6b4057376c3f4553fc0272a3
Sam Yu <samchenyu@gmail.com>
2024-05-28 21:45:59 +0800

    added transitions!

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-        // Generate an array of unique numbers from 1 to 100 and shuffle it
-        const data = Array.from({ length: 100 }, (_, i) => i + 1).sort(() => Math.random() - 0.5);
+        // Function to generate and shuffle data
+        function generateData() {
+            const numElements = document.getElementById('numElements').value;
+            return Array.from({ length: numElements }, (_, i) => i + 1).sort(() => Math.random() - 0.5);
+        }
+
+        // Initial data generation
+        let data = generateData();
 
         // Define the dimensions and margins for the chart
         const margin = { top: 20, right: 30, bottom: 40, left: 40 };
@@ -14,34 +20,62 @@
             .attr('transform', `translate(${margin.left},${margin.top})`);
 
         // Define the scales for x and y axes
-        const xScale = d3.scaleBand()
+        let xScale = d3.scaleBand()
             .domain(data.map((d, i) => i))
             .range([0, width])
             .padding(0.1);
 
-        const yScale = d3.scaleLinear()
+        let yScale = d3.scaleLinear()
             .domain([0, d3.max(data)])
             .nice()
             .range([height, 0]);
 
         // Create and append the x axis
-        svg.append('g')
+        const xAxis = svg.append('g')
             .attr('class', 'axis-x')
             .attr('transform', `translate(0, ${height})`)
             .call(d3.axisBottom(xScale).tickFormat(() => ''));
 
         // Create and append the y axis
-        svg.append('g')
+        const yAxis = svg.append('g')
             .attr('class', 'axis-y')
             .call(d3.axisLeft(yScale));
 
-        // Create and append the bars
-        svg.selectAll('.bar')
-            .data(data)
-            .enter()
-            .append('rect')
-            .attr('class', 'bar')
-            .attr('x', (d, i) => xScale(i))
-            .attr('y', d => yScale(d))
-            .attr('width', xScale.bandwidth())
-            .attr('height', d => height - yScale(d));
+        // Function to create and update bars with transitions
+        function createAndUpdateBars(data) {
+            // Update scales
+            xScale.domain(data.map((d, i) => i));
+            yScale.domain([0, d3.max(data)]).nice();
+
+            // Update axes
+            xAxis.call(d3.axisBottom(xScale).tickFormat(() => ''));
+            yAxis.call(d3.axisLeft(yScale));
+
+            // Bind data to bars
+            const bars = svg.selectAll('.bar')
+                .data(data);
+
+            // Remove old bars
+            bars.exit().remove();
+
+            // Create new bars and update existing bars with transitions
+            bars.enter()
+                .append('rect')
+                .attr('class', 'bar')
+                .merge(bars)
+                .transition()
+                .duration(1000)
+                .attr('x', (d, i) => xScale(i))
+                .attr('y', d => yScale(d))
+                .attr('width', xScale.bandwidth())
+                .attr('height', d => height - yScale(d));
+        }
+
+        // Initial bar creation
+        createAndUpdateBars(data);
+
+        // Event listener for the shuffle button
+        document.getElementById('shuffleButton').addEventListener('click', () => {
+            data = generateData();
+            createAndUpdateBars(data);
+        });
